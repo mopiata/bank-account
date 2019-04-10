005a3212df8a273206fa8b1d12d7785192666e26
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-04-10 13:19:45 +0300

    added constructor

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,20 @@ function BankAccount(name,initialDeposit){
 
 //user interface
 $(document).ready(function() {
-
   $("form#forms").submit(function(event) {
     event.preventDefault();
+
     var inputtedName = $("input#name").val();
     var inputtedInitialDeposit = $("input#initial_deposit").val();
     var newAccount = new BankAccount(inputtedName, inputtedInitialDeposit);
-  
-    $("#add-account").click(function() {
-      $("#balance").append(newAccount.name+ " "+newAccount.initialDeposit);
-    });
+    
+    var currentBalance;
+
+    $("#balance").append("<br><br>"+newAccount.initialDeposit);
+
+    // $("#add-account").click(function() {
+    //   $("#balance").append(newAccount.name+ " "+newAccount.initialDeposit);
+    // });
   });
 });
 
